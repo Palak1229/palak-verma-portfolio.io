df55c30dd749cd665fb1f5c554ace3524451a1a5
Palak Verma <88045393+Palak1229@users.noreply.github.com>
2025-01-24 00:25:33 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: ["<span> Hi, I'm Palak Verma </span> A passionate frontend developer dedicated to creating immersive and responsive digital experiences. With a blend of creativity and technical expertise, I transform ideas into visually appealing and user-friendly websites and applications.. Skilled in C, JAva, HTML, CSS, with multiple tools and technologies.B.Tech Focused In Computer Science From Gyan Ganga Intitute Of Technology And Science , Jabalpur Mp"],
+      typeSpeed: 01,
+      contentType: 'html'
+      
+    });
+  }, 1000);

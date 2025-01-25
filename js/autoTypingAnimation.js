710673e75b643ffa2300dd40189c35e6ef189b87
smartContact <palak.verma.cs19@ggits.net>
2025-01-25 12:16:53 +0530

    updated portfolio

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: ["<span> Hi, I'm Palak Verma </span> A passionate Backend Developer with a strong foundation in Java and a deep interest in crafting robust and scalable server-side solutions. With experience in building and optimizing backend systems, I specialize in designing APIs, managing databases, and ensuring seamless communication between applications and servers.My journey with Java has fueled my problem-solving mindset and inspired me to constantly explore new technologies to enhance backend functionality and performance. Whether it’s developing RESTful APIs, integrating databases, or tackling complex backend challenges, I thrive on delivering efficient and high-quality solutions.I am dedicated to continuous learning and always eager to collaborate on innovative projects that make an impact. Let’s build something amazing together!"],
+      typeSpeed: 1,
+      contentType: 'html'
+      
+    });
+  }, 1000);
